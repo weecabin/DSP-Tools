417001d5a9777d49ec174301a109adef8a7ef388
weecabin <weecabin-github@yahoo.com>
2022-05-24 05:56:14 +0000

    Added Correlation

--- Sources/app/app.swift
+++ Sources/app/app.swift
@@ -23,7 +23,7 @@ func Convolve(input:[Float],filter:[Float])->[Float]{
   let iLen = input.count 
   let fLen = filter.count
   var y:[Float]=[]
-  for i in (0...iLen+fLen-1){
+  for i in (0...iLen+fLen-2){
     y.append(Float(0))
     for j in (0...fLen-1){
       let k = i-j
@@ -35,9 +35,38 @@ func Convolve(input:[Float],filter:[Float])->[Float]{
   return y
 }
 
-let input:[Float]=[0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0]
-let impulse:[Float]=[0,1,0]
+func Correlate(input:[Float],filter:[Float])->[Float]{
+  let iLen = input.count 
+  let fLen = filter.count
+  var y:[Float]=[]
+  for i in (0...iLen-fLen){
+    y.append(Float(0))
+    for j in (0...fLen-1){
+      y[i] = y[i] + filter[j] * input[i+j]
+    }
+  }
+  return y
+}
 
-let y = Convolve(input:input,filter:sinc(from:-20,to:20,samples:25))
+let s = sinc(from:-20,to:20,samples:25)
+let pulse:[Float]=[0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0]
+let impulse:[Float]=[0,0,0,0,0,0,1,0,0]
+let s1:[Float]=[1,1,1,1,1,1,1,1,1,2,3,4,5,1,1,1,1,1,1,1,1,1]
+let s2:[Float]=[1,1,1,1,5,4,3,2,1,1,1,1]
+let s3:[Float]=[1,2,3,4,5,1]
+let s4:[Float]=[1,5,4,3,2,1]
+let s5:[Float]=[1,1,1,2,3,4,5,1,1,1,1,1,1,1,1,1]
+
+print("Convolution")
+let y = Convolve(input:s,filter:impulse)
 print(y)
+print("in: \(pulse.count) flit: \(impulse.count) out: \(y.count)")
+print("ramp against itself")
+print(Convolve(input:s1,filter:s1))
+print("ramp against flipped ramp")
+print(Convolve(input:s1,filter:s2))
+print(Convolve(input:s5,filter:s4))
 
+print("correlate")
+print(Correlate(input:s1,filter:s3))
+print(Correlate(input:s5,filter:s3))
