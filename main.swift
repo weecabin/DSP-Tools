8fc502e84934b90158754aa9aa21a9ca6fdb6983
weecabin <weecabin-github@yahoo.com>
2022-06-14 15:28:20 +0000

    added Cos function

--- main.swift
+++ main.swift
@@ -9,16 +9,20 @@ func Step(x:Float)->Float{
     if x>=10{return 1}
     else{return 0}
 }
+func Cos(w:Int,i:Int)->Float{
+    let delta = 2.0*Float.pi/Float(w)
+    return cos(Float(i)*delta)
+}
 let rampPeak = Float(20)
 var rampFilt = line(firsty:0,lasty:rampPeak,values:3)
 rampFilt.append(contentsOf:line(firsty:rampPeak,lasty:0,values:3,outStartIndex:1))
 
 var hpf = Hpf()
 var lpf = Lpf()
-var filt = Filt(ay1:-0.9,bx0:0.1,bx1:0.7)
+var filt = Filt(ay1:-0.9,bx0:0.1,bx1:-0.7)
 var x:[Float]=[]
 for i in (0...75){
-    x.append(Step(x:Float(i)))
+    x.append(Cos(w:10,i:i))
 }
 
 var res:String=""
